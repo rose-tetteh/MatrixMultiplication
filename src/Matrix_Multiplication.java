14dd7a96435f5bdf2874fe34db7444ca90c06bb4
rose-tetteh <rose.tetteh@amalitech.com>
2024-11-11 15:49:55 +0000

    refactor

--- src/Matrix_Multiplication.java
+++ src/Matrix_Multiplication.java
@@ -12,7 +12,7 @@ public class Matrix_Multiplication {
      */
     public static int[][] getMatrix(int row, int column){
         int[][] matrix = new int[row][column];
-        System.out.println("Enter the values of the matrix row by row: eg 2 3 4 go to the next line and enter values for the next row: ");
+        System.out.println("Enter the values of the matrix row by row: eg 2 3 4 go to the next line and enter values for the next row Note: any extra score will not be considered: ");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 matrix[i][j] = scanner.nextInt();
